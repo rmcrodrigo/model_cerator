d298783e1383ff66402a76a4f2bfb3b18f25feee
Rodrigo Montiel <rmcrodrigo1@gmail.com>
2019-08-29 06:58:26 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelClassBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
